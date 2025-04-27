961570049a4e7a7024dd68e7c3d9d9a6018a8416
Lydell Dsouza <lydelldsouza24@gmail.com>
2025-04-27 20:37:13 +0530

    changedThe code

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,14 +1,21 @@
 import FileUploader from './components/FileUploader';
-import './App.css'
+import './App.css';
+import SplineBackground from './components/SplineBackground';
 
 function App() {
-  
-
   return (
-    <div className="min-h-screen bg-gray-50 flex items-center justify-center">
-      <FileUploader />
+    <div>
+      <div className="spline-background">
+        <SplineBackground />
+      </div>
+      <div className="page-wrapper">
+        <h1 className="Heading">Resume Grader</h1>
+        <div className="main-content">
+          <FileUploader />
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App
+export default App;
---
+++ frontEnd/src/components/SplineBackground.jsx
@@ -0,0 +1,9 @@
+import Spline from '@splinetool/react-spline';
+
+function SplineBackground() {
+  return (
+    <Spline scene="https://prod.spline.design/WRklCbCSd2IuieLr/scene.splinecode" />
+  );
+}
+
+export default SplineBackground;
